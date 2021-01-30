32381073dd31b1529518b608cacd610c3452cd4e
[wanwanbree] <[15578303150@163.com]>
2021-01-30 12:47:22 +0800

    增加setup和teardown练习

--- test_work1.py
+++ test_work1.py
@@ -5,20 +5,54 @@
 
 from fun.calculation import Calulation
 import pytest
+import sys
+sys.path.append('.')
+def setup_module():
+    print("每个模块内执行一次")
 
-cal = Calulation()
+def teardown_module():
+    print("每个模块内执行一次")
 
-@pytest.mark.parametrize('a, b, c',[(1,2,3),(1,2,4),['a',2,5]])
-def test_add(a, b, c):
-    assert c==cal.myadd(a,b)
-@pytest.mark.parametrize('a, b, c',[(1,0,0),(2,1,2.0)])
-def test_devision(a,b,c):
-    assert c==cal.division(a,b)
-@pytest.mark.parametrize('a,b,c',[(2,21,42),(0,2,0)])
-def test_mul(a,b,c):
-    assert c==cal.multiplication(a,b)
+def setup_function():
+    print("不在类内的每个用例内开始执行一次")
 
+def teardown_function():
+    print("不在类中的每个用例结束时执行")
 
+def test_01():
+    pass
+def test_02():
+    pass
 
+class TestWork:
 
+    def setup_class(self):
+        print("类内执行一次开始")
+
+    def teardown_class(self):
+        print("类内执行一次结束")
+
+    def setup_method(self):
+        print("每个用例执行开始")
+
+    def teardown_method(self):
+        print("用例执行结束")
+
+    def setup(self):
+        print("每个用例执行一次")
+
+    def teardown(self):
+        print("每个用例结束执行")
+
+    cal = Calulation()
+
+    @pytest.mark.parametrize('a, b, c',[(1,2,3),(1,2,4),['a',2,5]])
+    def test_add(self,a, b, c):
+        assert c==self.cal.myadd(a,b)
+    @pytest.mark.parametrize('a, b, c',[(1,0,0),(2,1,2.0)])
+    def test_devision(self,a,b,c):
+        assert c==self.cal.division(a,b)
+    @pytest.mark.parametrize('a,b,c',[(2,21,42),(0,2,0)])
+    def test_mul(self,a,b,c):
+        assert c==self.cal.multiplication(a,b)
 
